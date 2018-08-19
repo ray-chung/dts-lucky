328425b86830888255a809fefc6d30f3cb2cbdcf
raychung <raychung.tw@gmail.com>
2018-08-19 22:01:46 +0800

    make it better

--- database.py
+++ database.py
@@ -3,9 +3,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-current_dir = os.path.dirname(__file__)
-
-engine = create_engine('sqlite:////{}/luckymember.db'.format(current_dir), convert_unicode=True)
+engine = create_engine('postgresql://{user}:{pwd}@{host}/{db}'.format(user=os.environ.get('db_user'),
+                                                                      pwd=os.environ.get('db_pwd'),
+                                                                      host=os.environ.get('db_host'),
+                                                                      db=os.environ.get('db_name')),
+                       convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          expire_on_commit=False,
