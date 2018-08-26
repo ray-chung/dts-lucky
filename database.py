3f44b60ef0584ce8967aedbd99df3fb8f7655ddb
Ray Chung <raychung.tw@gmail.com>
2018-08-26 17:45:24 +0800

    Update database.py

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
+import json
 
 
 f = open('secret.json', 'r')
